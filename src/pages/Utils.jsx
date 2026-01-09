39cbeeea647a7b39e3029cb01caf4694f9c567b7
Tetsu <ttgchang@gmail.com>
2026-01-09 10:15:01 +0800

    update Utils.jsx

--- src/pages/Utils.jsx
+++ src/pages/Utils.jsx
@@ -1,6 +1,6 @@
 export default function Utils() {
     return (
-    <>
+    <section class="container mt-10">
         <h2 className="mb-6">文字（Typograghy）</h2>
         <div className="d-flex gap-8">
             <div className="mb-8 text-success w-100">
@@ -32,7 +32,40 @@ export default function Utils() {
                     </tr>
                 </table>
             </div>
+            <div className="mb-8 text-success w-100">
+                <h6 className="mb-4">文字（Text）</h6>
+                <table className="table table-striped">
+                    <tr>
+                        <td><p className="text-p-24">P-L 愛哆啦也愛手作</p></td>
+                        <td><code>text-p-24</code></td>
+                    </tr>
+                    <tr>
+                        <td><p className="text-p-20-b">P-M-B 愛哆啦也愛手作</p></td>
+                        <td><code>text-p-20-b</code></td>
+                    </tr>
+                    <tr>
+                        <td><p className="text-p-20-r">P-M-R 愛哆啦也愛手作</p></td>
+                        <td><code>text-p-20-r</code></td>
+                    </tr>
+                    <tr>
+                        <td><p className="text-p-16-b">P-R-B 愛哆啦也愛手作</p></td>
+                        <td><code>text-p-16-b</code></td>
+                    </tr>
+                    <tr>
+                        <td><p className="text-p-16-r">P-R-R 愛哆啦也愛手作</p></td>
+                        <td><code>text-p-16-r</code></td>
+                    </tr>
+                    <tr>
+                        <td><p className="text-p-14-b">P-S-B 愛哆啦也愛手作</p></td>
+                        <td><code>text-p-14-b</code></td>
+                    </tr>
+                    <tr>
+                        <td><p className="text-p-14-r">P-S-R 愛哆啦也愛手作</p></td>
+                        <td><code>text-p-14-r</code></td>
+                    </tr>
+                </table>
+            </div>
         </div>
-    </>
+    </section>
     )
 }
