fd60a75313f20d91b0901c4d69503ccca8f4b6f8
paytonwu90 <sw9008k@gmail.com>
2026-02-12 07:06:13 +0800

    新增 ProductCard 元件
    - 新增 ProductCard 元件
    - 在 vite.config.js 新增 alias 方便 import
    - 刪除用不到的 vite template 樣式

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,31 @@
+import { Heart, ShoppingCart } from "lucide-react";
+
+function ProductCard({ product }) {
+  return (
+    <div className="product-card">
+      <div className={`position-relative ${product.bgClass} p-4 rounded-4 mb-4`}>
+        <img src={product.image} className="w-100" alt={`新品上架-${product.name}`} />
+        <div className="product-card__like">
+          <button type="button" className="d-block bg-gray-50 p-4 rounded-5">
+            <Heart />
+          </button>
+        </div>
+      </div>
+      <div className="product-card-body">
+        <div className="d-flex align-items-center justify-content-between">
+          <div>
+            <h5 className="text-p-24-b text-gray-600">{product.name}</h5>
+            <p className="text-p-20-b text-secondary-700 mb-0">NT${product.price}</p>
+          </div>
+          <div className="p-3">
+            <button type="button" className="product-card__cart d-block bg-transparent p-0">
+              <ShoppingCart strokeWidth={2.5} className="text-secondary-700" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard;
