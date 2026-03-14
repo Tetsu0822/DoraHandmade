78f936bb0885e95161909675f410117ce527e1c5
Tetsu <ttgchang@gmail.com>
2026-03-14 21:26:54 +0800

    feat: 後台

--- src/layout/BackendLayout.jsx
+++ src/layout/BackendLayout.jsx
@@ -1,8 +1,85 @@
+import { useForm } from "react-hook-form";
+import { emailValidation } from "../utils/validation";
+const ADMIN_USERNAME = import.meta.env.VITE_ADMIN_USERNAME;
+const ADMIN_PASSWORD = import.meta.env.VITE_ADMIN_PASSWORD;
 import { Link, Outlet } from "react-router";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import axios from 'axios'
+const VITE_API_BASE = import.meta.env.VITE_API_BASE;
+const VITE_API_PATH = import.meta.env.VITE_API_PATH;
 
 const BackendLayout = () => {
+    const navigate = useNavigate();
+    const [ isLogin, setIsLogin ] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm({
+        mode: "onChange",
+        defaultValues: {
+            username: "",
+            password: "",
+        }
+    });
+
+    const onSubmit = async (formData) => {
+        try {
+            console.log("預設資料:", ADMIN_PASSWORD, ADMIN_USERNAME);
+            console.log("登入資料:", formData);
+            if (formData.username === ADMIN_USERNAME && formData.password === ADMIN_PASSWORD) {
+                setIsLogin(true);
+                localStorage.setItem("adminLogin", "true");
+
+            // 登入六角API
+            const response = await axios.post(`${VITE_API_BASE}/admin/signin`, formData);
+            console.log("登入成功:", response.data);
+            const setCookie = () => {
+                const { token, expired } = response.data;
+                document.cookie = `hexToken=${token};expires=${new Date(expired)};`;
+                axios.defaults.headers.common.Authorization = token;
+            };
+            setCookie();
+            // getProducts();
+            // setIsAuth(true);
+            // alert("登入成功: " + response.data.message);
+            navigate("/admin/product");
+            } else {
+                alert("帳號或密碼錯誤");
+            }
+
+            // const loginData = {
+            //     username: formData.username,
+            //     password: formData.password,
+            // };
+            // const response = await axios.post(`${VITE_API_BASE}${VITE_API_PATH}/admin/signin`, loginData);
+            // if (response.data.success) {
+            //     setIsLogin(true);
+            //     localStorage.setItem("adminLogin", "true");
+            // } else {
+            //     alert(response.data.message);
+            // }
+        } catch (error) {
+            console.error(error);
+            alert("登入失敗，請稍後再試");
+        }
+    };
+
+    // 初始化時自動讀取 localStorage 判斷是否已登入
+    useEffect(() => {
+        if (localStorage.getItem("adminLogin") === "true") {
+            setTimeout(() => {
+                setIsLogin(true);
+            }, 0); // 延遲設置狀態，避免 React 警告
+        }
+    }, []);
+
     return (
         <>
+        {/* // 如果已登入顯示後台選單，否則顯示登入選單 */}
+        {isLogin ? (
+            <>
             <header>
                 <ul className="nav">
                     <li className="nav-item">
@@ -14,12 +91,70 @@ const BackendLayout = () => {
                     <li className="nav-item">
                         <Link className="nav-link" to="/admin/order">訂單管理</Link>
                     </li>
+                    <li className="nav-item">
+                        <button className="btn btn-link nav-link" onClick={() => {
+                            setIsLogin(false);
+                            localStorage.removeItem("adminLogin");
+                        }}>登出</button>
+                    </li>
                 </ul>
             </header>
             <main>
                 <Outlet />
             </main>
             <footer></footer>
+            </>
+        ) : (
+            <div className="container py-15 login">
+                <div className="row justify-content-center">
+                <h1 className="h3 mb-3 font-weight-normal text-center">請先登入</h1>
+                <div className="col-8">
+                    <form id="form" className="form-signin" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-floating mb-3">
+                        <input
+                        type="email"
+                        className="form-control"
+                        id="username"
+                        placeholder="name@example.com"
+                        {...register("username", emailValidation)}
+                        />
+                        <label htmlFor="username">Email address</label>
+                        {errors.username && (
+                        <div className="text-danger mt-1">{errors.username.message}</div>
+                        )}
+                    </div>
+                    <div className="form-floating">
+                        <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Password"
+                        {...register("password", {
+                            required: "請輸入密碼",
+                            minLength: {
+                                value: 6,
+                                message: "密碼長度至少需 6 碼",
+                            },
+                            })}
+                        />
+                        <label htmlFor="password">Password</label>
+                        {errors.password && (
+                        <div className="text-danger mt-1">{errors.password.message}</div>
+                        )}
+                    </div>
+                    <button
+                        className="btn btn-lg btn-primary w-100 mt-3"
+                        type="submit"
+                        disabled={!isValid}
+                        >
+                        登入
+                    </button>
+                    </form>
+                </div>
+                </div>
+                <p className="mt-5 mb-3 text-muted text-center">&copy; 2026~∞ - 愛哆啦也愛手作</p>
+            </div>
+        )}
         </>
     )
 }
--- src/pages/backend/AdminProducts.jsx
+++ src/pages/backend/AdminProducts.jsx
@@ -1,8 +1,170 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+import * as bootstrap from 'bootstrap';
+import { useRef } from "react";
+import ProductModal from '@components/backend/ProductModal';
+import Pagination from '@components/backend/Pagination';
+// import { useMessage } from "../../hooks/useMessage";
+const VITE_API_BASE = import.meta.env.VITE_API_BASE;
+const VITE_API_PATH = import.meta.env.VITE_API_PATH;
+const INITIAL_TEMPLATE_DATA = {
+    id: "",
+    title: "",
+    category: "",
+    origin_price: "",
+    price: "",
+    unit: "",
+    description: "",
+    content: "",
+    is_enabled: false,
+    imageUrl: "",
+    imagesUrl: [],
+    parentCategory: "",
+    features: "",
+    specifications: "",
+    is_hot: false,
+    is_new: false,
+    published_at: "",
+};
+
 function AdminProducts() {
+    // const [isAuth, setIsAuth] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [templateProduct, setTemplateProduct] = useState(INITIAL_TEMPLATE_DATA);
+    const [ modalType, setModalType ] = useState(""); // 'create', 'edit', 'delete'
+    const [ pagination, setPagination ] = useState({});
+    const [currentPage, setCurrentPage] = useState(1);// 目前頁碼
+    const productModalRef = useRef(null);
+    // const { showError, showSuccess } = useMessage();
+
+    // 取得產品列表的 API 呼叫
+    const fetchProducts = async (page = 1) => {
+        try {
+            const token = document.cookie.replace(
+                /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
+                "$1"
+            );
+            axios.defaults.headers.common.Authorization = token;
+            const response = await axios.get(`${VITE_API_BASE}/api/${VITE_API_PATH}/admin/products?page=${page}`, {
+                headers: {
+                    Authorization: token,
+                },
+            });
+            console.log("產品列表:", response.data.products);
+            setProducts(response.data.products);
+            setPagination(response.data.pagination);
+            setCurrentPage(page);
+        } catch (error) {
+            console.error("取得產品列表失敗:", error);
+        }
+    };
+
+    const openProductModal = (type, product) => {
+        setModalType(type);
+        setTemplateProduct(() => ({
+        ...INITIAL_TEMPLATE_DATA,
+        ...product,
+        imagesUrl: (product.imagesUrl && product.imagesUrl.length > 0) ? product.imagesUrl : [""],
+        }));
+        productModalRef.current.show();
+    };
+
+    const closeProductModal = () => {
+        productModalRef.current.hide();
+    };
+
+    useEffect(() => {
+        productModalRef.current = new bootstrap.Modal('#productModal', {
+            keyboard: false
+        });
+
+        // Modal 關閉時移除焦點
+        document
+        .querySelector("#productModal")
+        .addEventListener("hide.bs.modal", () => {
+        if (document.activeElement instanceof HTMLElement) {
+            document.activeElement.blur();
+        }
+        });
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        fetchProducts();
+    }, []);
+
     return (
+        <>
+      <h2>產品管理</h2>
+      <div>
         <div className="container">
-            <h1>商品管理</h1>
+          <div className="text-end mt-4">
+            <button
+              className="btn btn-primary"
+              onClick={() => openProductModal("create", INITIAL_TEMPLATE_DATA)}>
+                建立新的產品
+            </button>
+          </div>
+          <table className="table mt-4">
+            <thead>
+              <tr>
+                <th width="120">父分類</th>
+                <th width="120">子分類</th>
+                <th>產品名稱</th>
+                <th width="120">原價</th>
+                <th width="120">售價</th>
+                <th width="100">是否啟用</th>
+                <th width="120">發布日期</th>
+                <th width="120">編輯</th>
+              </tr>
+            </thead>
+            <tbody>
+            {products.map((item) => (
+              <tr key={item.id}>
+                <td>{item.parentCategory}</td>
+                <td>{item.category}</td>
+                <td>{item.title}</td>
+                <td className="text-end">{item.origin_price}</td>
+                <td className="text-end">{item.price}</td>
+                <td>
+                  {item.is_enabled ? (
+                    <span className="text-success">啟用</span>
+                  ) : (
+                    <span>未啟用</span>
+                  )}
+                </td>
+                <td>{new Date(item.published_at).toLocaleDateString()}</td>
+                <td>
+                  <div className="btn-group">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary btn-sm"
+                      onClick={() => openProductModal("edit", item)}
+                    >
+                      編輯
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-outline-danger btn-sm"
+                      onClick={() => openProductModal("delete", item)}
+                    >
+                      刪除
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+          <Pagination pagination={pagination} onPageChange={fetchProducts} />
         </div>
+      </div>
+
+      <ProductModal
+        modalType={modalType}
+        templateProduct={templateProduct}
+        getProducts={fetchProducts}
+        currentPage={currentPage}
+        closeProductModal={closeProductModal}
+      />
+    </>
     )
 }
 
